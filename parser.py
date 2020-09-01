8dc14095326325cc2b6c12bc1134f5817102485a
Peter Gemmell <petergemmell65@hotmail.com>
2020-09-01 12:18:19 +0100

    ticker variable complete

--- parser.py
+++ parser.py
@@ -3,3 +3,31 @@ import pandas as pd
 from yahoo_fin import stock_info as si
 from pandas_datareader import DataReader
 import numpy as np
+
+tickers = si.tickers_sp500()
+recommendations = []
+
+
+for ticker in tickers:
+    lhs_url = 'https://query2.finance.yahoo.com/v10/finance/quoteSummary/'
+    rhs_url = '?formatted=true&crumb=swg7qs5y9UP&lang=en-US&region=US&' \
+              'modules=upgradeDowngradeHistory,recommendationTrend,' \
+              'financialData,earningsHistory,earningsTrend,industryTrend&' \
+              'corsDomain=finance.yahoo.com'
+
+    url = lhs_url + ticker + rhs_url
+    r = requests.get(url)
+    if not r.ok:
+        recommendations = 6
+    try:
+        result = r.json()['quoteSummary']['result'][0]
+        recommendation = result['financialData']['recommendationMean']['fmt']
+    except:
+        recommendation = 6
+
+
+    recommendations.append(recommendation)
+
+    print("--------------------------------------------")
+    print("{} has an average recommendation of: ".format(ticker), recommendation)
+    # time.sleep(0.5)
