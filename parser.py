e02545050b4425d7a1839d3a9746c6969c70b4a6
Peter Gemmell <petergemmell65@hotmail.com>
2020-09-01 16:34:27 +0100

    dataframe now saving as csv file with recommendations

--- parser.py
+++ parser.py
@@ -7,7 +7,6 @@ import numpy as np
 tickers = si.tickers_sp500()
 recommendations = []
 
-
 for ticker in tickers:
     lhs_url = 'https://query2.finance.yahoo.com/v10/finance/quoteSummary/'
     rhs_url = '?formatted=true&crumb=swg7qs5y9UP&lang=en-US&region=US&' \
@@ -15,26 +14,24 @@ for ticker in tickers:
               'financialData,earningsHistory,earningsTrend,industryTrend&' \
               'corsDomain=finance.yahoo.com'
 
-    url = lhs_url + ticker + rhs_url
+    url =  lhs_url + ticker + rhs_url
     r = requests.get(url)
     if not r.ok:
-        recommendations = 6
+        recommendation = 6
     try:
         result = r.json()['quoteSummary']['result'][0]
-        recommendation = result['financialData']['recommendationMean']['fmt']
+        recommendation =result['financialData']['recommendationMean']['fmt']
     except:
         recommendation = 6
-
 
     recommendations.append(recommendation)
 
     print("--------------------------------------------")
-    print("{} has an average recommendation of: ".format(ticker), recommendation)
-    # time.sleep(0.5)
-
+    print ("{} has an average recommendation of: ".format(ticker), recommendation)
+    #time.sleep(0.5)
 
 dataframe = pd.DataFrame(list(zip(tickers, recommendations)), columns =['Company', 'Recommendations'])
 dataframe = dataframe.set_index('Company')
 dataframe.to_csv('recommendations.csv')
 
-print(df)
+# print (df)
